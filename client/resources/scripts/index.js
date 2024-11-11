3792e5c6da9572797fdc63eb342a3e4bb3cdbd00
Matthew Simpson <jmsimpson7@crimson.ua.edu>
2024-11-10 20:17:19 -0600

    url updated

--- client/resources/scripts/index.js
+++ client/resources/scripts/index.js
@@ -1,5 +1,5 @@
 let myMerch = []
-const url = "http://localhost:5217/api/Merch"
+const url = "http://localhost:5195/api/WeatherForecast"
 async function HandleOnLoad(){
     await GetAllMerch()
     console.log(myMerch)

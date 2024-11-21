e95c604e1c4b8473f0b7b8ef1cdac87b387ee9d7
Andrew Dortce <you@example.com>
2024-11-21 12:43:19 -0600

    Controllers

---
+++ api/Controllers/ItemController.cs
@@ -0,0 +1,74 @@
+// using Microsoft.AspNetCore.Mvc;
+
+// namespace api.Controllers;
+
+// [Route("[controller]")]
+// [ApiController]
+// public class ItemController : ControllerBase
+// {
+//     [HttpGet("{id}", Name ="Get")]
+//     public async Task<List<ItemController>> Get()
+//     {
+//         Database myDatabase = new();
+
+//         return await myDatabase.GetAllItems();
+//     }
+
+//     [HttpPost]
+//     public async void Post([FromBody] Item value)
+//     {
+//         System.Console.WriteLine(value.Name);
+//         Database myDatabase = new();
+//         await myDatabase.InsertItem(value);
+//     }
+// }
+
+
+using api.database;
+using api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ItemController : ControllerBase
+{
+    //GET: api/recipe
+    [HttpGet]
+    public async Task<List<Item>> GetRecipes()
+    {
+        Database myDatabase = new();
+        
+        return await myDatabase.GetAllItems();
+    }
+
+    [HttpGet("{id}", Name="Get")]
+    public async Task<List<Item>> GetItem(int id)
+    {
+        Database myDatabase = new();
+
+        return await myDatabase.GetItem(id);
+    }
+
+        [HttpPost]
+        public async Task Post([FromBody]Item value)
+        {
+            Database myDatabase = new();
+            await myDatabase.InsertItem(value);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            Database myDatabase = new();
+            await myDatabase.DeleteItem(id);
+        }
+
+        [HttpPut("{id}")]
+        public async Task Put(int id, [FromBody]Item value)
+        {
+            Database myDatabase = new();
+            await myDatabase.UpdateItem(value, id);
+        }
+}
--- api/Databases/Database.cs
+++ api/Databases/Database.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using api.Models;
 using MySqlConnector;
 
 namespace api.Database
@@ -13,6 +14,105 @@ namespace api.Database
             cs = "Server=ryvdxs57afyjk41z.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;User ID=qxd5a8g1n496kpfb;Password=awpw92wy7ya8yp15;Database=vgo20llik8rocjos;Port=3306";
         }
 
-        
+        public async Task<List<Item>> SelectItem(string sql, List<MySqlParameter> parms){
+
+            List<Item> myItem = new();
+            using var connection = new MySqlConnection(cs);
+            await connection.OpenAsync();
+            using var command = new MySqlCommand(sql, connection);
+
+            if(parms != null){
+                command.Parameters.AddRange(parms.ToArray());
+            }
+
+            using var reader = await command.ExecuteReaderAsync();
+            while (await reader.ReadAsync())
+            {
+                myItem.Add(new Item(){
+                    ID = reader.GetInt32(0),
+                    Name = reader.GetString(1),
+                    Team = reader.GetString(2),
+                    Sport = reader.GetString(3),
+                    Status = reader.GetString(4),
+                    Size = reader.GetString(5),
+                    yearMade = reader.GetInt32(6),
+                    price = reader.GetDouble(7),
+                    category = reader.GetString(8),
+                    nameOfPlayer = reader.GetString(9),
+                    purchaseID = reader.GetInt32(10),
+                    quantity = reader.GetInt32(11)
+                });
+            }
+
+            return myItem;
+        }
+
+        private async Task ItemNoReturnSql(string sql, List<MySqlParameter> parms){
+            List<Item> myItem = new();
+            using var connection = new MySqlConnection(cs);
+            await connection.OpenAsync();
+            using var command = new MySqlCommand(sql, connection);
+
+            if(parms != null){
+                command.Parameters.AddRange(parms.ToArray());
+            }
+
+            await command.ExecuteNonQueryAsync();
+        }
+
+        public async Task<List<Item>> GetAllPurchases(){
+            string sql = "SELECT * FROM purchase;";
+            List<MySqlParameter> parms = new();
+            return await SelectItem(sql, parms);
+        }
+
+        public async Task<List<Item>> GetAllProducts(){
+            string sql = "SELECT * FROM product WHERE quantity > 0;";
+            List<MySqlParameter> parms = new();
+            return await SelectItem(sql, parms);
+        }
+
+        public async Task<List<Item>> GetAllEmployees(){
+            string sql = "SELECT * FROM employee;";
+            List<MySqlParameter> parms = new();
+            return await SelectItem(sql, parms);
+        }
+
+        public async Task<List<Item>> GetAllCustomers(){
+            string sql = "SELECT * FROM customer;";
+            List<MySqlParameter> parms = new();
+            return await SelectItem(sql, parms);
+        }
+
+        public async Task InsertPurchase(Item item){
+            string sql = @$"INSERT INTO purchase (purchaseID, purchaseDate, pointsEarned)
+                            VALUES (@purchaseID, @purchaseDate, @pointsEarned);";
+            List<MySqlParameter> parms = new();
+            parms.Add(new MySqlParameter("@purchaseID", MySqlDbType.String) {Value = item.ID});
+            parms.Add(new MySqlParameter("@purchaseDate", MySqlDbType.Date) {Value = DateTime.Now.ToString()});
+            parms.Add(new MySqlParameter("@pointsEarned", MySqlDbType.Int32) {Value = Math.Round((item.price * 10), 0)});
+            await ItemNoReturnSql(sql, parms);
+            
+        }
+
+        public async Task InsertProduct(Item item){
+            string sql = @$"INSERT INTO product (productID, productName, price, status, team, category, sport, quantity, yearMade, size, nameOfPlayer)
+                            VALUES (@productID, @productName, @price, @status, @team, @category, @sport, @quantity, @yearMade, @size, @nameOfPlayer);";
+            List<MySqlParameter> parms = new();
+            parms.Add(new MySqlParameter("@productID", MySqlDbType.String) {Value = item.ID});
+            parms.Add(new MySqlParameter("@productName", MySqlDbType.Date) {Value = item.Name});
+            parms.Add(new MySqlParameter("@price", MySqlDbType.Int32) {Value = item.price});
+            parms.Add(new MySqlParameter("@status", MySqlDbType.Int32) {Value = item.Status});
+            parms.Add(new MySqlParameter("@team", MySqlDbType.Int32) {Value = item.Team});
+            parms.Add(new MySqlParameter("@category", MySqlDbType.Int32) {Value = item.category});
+            parms.Add(new MySqlParameter("@sport", MySqlDbType.Int32) {Value = item.Sport});
+            parms.Add(new MySqlParameter("@quantity", MySqlDbType.Int32) {Value = item.quantity});
+            parms.Add(new MySqlParameter("@yearMade", MySqlDbType.Int32) {Value = item.yearMade});
+            parms.Add(new MySqlParameter("@size", MySqlDbType.Int32) {Value = item.Size});
+            parms.Add(new MySqlParameter("@nameOfPlayer", MySqlDbType.Int32) {Value = item.nameOfPlayer});
+            await ItemNoReturnSql(sql, parms);
+            
+        }
+    
     }
 }

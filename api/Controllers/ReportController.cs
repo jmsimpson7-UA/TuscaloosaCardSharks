db00964eb0e20a13f82d0eab8c64cf350ae0c2c9
gcneil <gcneil@crimson.ua.edu>
2024-12-03 16:41:44 -0600

    report controllers

---
+++ api/Controllers/ReportController.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Controllers
+{
+    public class ReportController
+    {
+        public async Task<List<Purchase>> getRewardsReport(){
+
+            Database myDatabase = new();
+
+            return await myDatabase.RewardsReport();            
+        }
+
+        public async Task<List<Purchase>> getSportsReport(){
+
+            Database myDatabase = new();
+
+            return await myDatabase.QuarterlyReport();            
+        }
+
+        public async Task<List<Purchase>> getFirstQuarterReport(){
+
+            Database myDatabase = new();
+
+            return await myDatabase.Q1PurchaseReport();            
+        }
+
+        public async Task<List<Purchase>> getSecondQuarterReport(){
+
+            Database myDatabase = new();
+
+            return await myDatabase.Q2PurchaseReport();            
+        }
+
+        public async Task<List<Purchase>> getThirdQuarterReport(){
+
+            Database myDatabase = new();
+
+            return await myDatabase.Q3PurchaseReport();            
+        }
+
+        public async Task<List<Purchase>> getFourthQuarterReport(){
+
+            Database myDatabase = new();
+
+            return await myDatabase.Q4PurchaseReport;            
+        }
+
+        public async Task<List<Purchase>> getAllSalesReport(){
+
+            Database myDatabase = new();
+
+            return await myDatabase.QuarterlyReport();            
+        }
+    }
+}

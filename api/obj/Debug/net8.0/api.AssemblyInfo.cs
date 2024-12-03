3b7e83352afd0e9e923da9956be717cf7ad17224
Matthew Simpson <jmsimpson7@crimson.ua.edu>
2024-12-03 16:32:56 -0600

    pre pull

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c59499d99fde93e291cb967f77f7bb62f83f215f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+457422053def08d5b6fb215c039b5c8453091271")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

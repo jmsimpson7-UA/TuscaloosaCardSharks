e919a3d09ec70cb97dd9fe7607210fcfdf8431fa
Matthew Simpson <jmsimpson7@crimson.ua.edu>
2024-12-04 11:38:28 -0600

    Pre switch

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54200ab9cd76ce508eb8008fcfd51006b443e4c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80a798da9e0f7f24dd7830b5cd1557ee5ababcc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

d4ab14eecbbc76082cd30fcb659f6f2ed5ff5e15
Andrew Dortce <you@example.com>
2024-12-04 08:39:05 -0600

    Employee and Inventory manager functions

--- client/resources/scripts/rewardseditor.js
+++ client/resources/scripts/rewardseditor.js
@@ -4,22 +4,20 @@ document.addEventListener("DOMContentLoaded", () => {
     const saveButton = document.getElementById("save-button");
     const successMessage = document.getElementById("success-message");
 
-
     let pointsPerDollar = 10;
-
 
     currentPointsElement.textContent = pointsPerDollar;
 
-
     saveButton.addEventListener("click", () => {
-        const newPoints = parseInt(pointsInput.value, 10); // Get the value from input
-
+        const newPoints = parseInt(pointsInput.value, 10);
 
         if (newPoints >= 1 && newPoints <= 100) {
             pointsPerDollar = newPoints; 
-            currentPointsElement.textContent = pointsPerDollar; 
-        } else {
-            alert("Please enter a valid number between 1 and 10."); 
+            currentPointsElement.textContent = pointsPerDollar;
+
+            successMessage.classList.remove("hidden");
+            successMessage.textContent = "Changes saved successfully!";
         }
+        
     });
 });

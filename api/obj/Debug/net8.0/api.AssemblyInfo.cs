19931df29464d98925589fb3ea051abdc2c26922
Matthew Simpson <jmsimpson7@crimson.ua.edu>
2024-12-04 00:29:27 -0600

    pre merge

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d58ccc8ebd2bb241f73dd41a3bc7e1c297b2a5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58be679623a84e62f934e2c41babc22b5e0fbbdb")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
